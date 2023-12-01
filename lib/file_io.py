45e1c87dca14bcd096d76c99c847ad58485bf71e
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-01 13:19:12 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,36 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    """
+    Writes the given content to a text file with the specified name.
+
+    Args:
+    - file_name (str): The name of the file (including the extension).
+    - file_content (str): The content to be written to the file.
+    """
+    with open(f'{file_name}.txt', 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Appends the given content to a text file with the specified name.
+
+    Args:
+    - file_name (str): The name of the file (including the extension).
+    - append_content (str): The content to be appended to the file.
+    """
+    with open(f'{file_name}.txt', 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """
+    Reads the content of a text file with the specified name.
+
+    Args:
+    - file_name (str): The name of the file (including the extension).
+
+    Returns:
+    - str: The content of the file.
+    """
+    with open(f'{file_name}.txt', 'r') as file:
+        return file.read()
--- lib/testing/file_io_test.py
+++ lib/testing/file_io_test.py
@@ -1,31 +1,22 @@
-from file_io import write_file, append_file, read_file
+from lib.file_io import write_file, append_file, read_file
 
-def test_write_file(tmp_path):
-    """Test write_file()"""
+def test_write_and_read_file(tmp_path):
     file_name = tmp_path / "test_file"
     file_content = "This is a test content."
+
     write_file(file_name, file_content)
-    with open(f'{file_name}.txt', 'r') as f:
-        file_content_read = f.read()
-    assert file_content_read == file_content
 
-def test_append_file(tmp_path):
-    """Test append_file()"""
+    result = read_file(file_name)
+    assert result == file_content
 
+def test_append_and_read_file(tmp_path):
     file_name = tmp_path / "test_file"
     file_content = "This is a test content."
     append_content = "\nAppended content."
+
     write_file(file_name, file_content)
     append_file(file_name, append_content)
-    with open(f'{file_name}.txt', 'r') as f:
-        file_content_read = f.read()
-    assert file_content_read == file_content + append_content
 
-def test_read_file(tmp_path):
-    """Test read_file()"""
-
-    file_name = tmp_path / "test_file"
-    file_content = "This is a test content."
-    write_file(file_name, file_content)
-    file_content_read = read_file(file_name)
-    assert file_content_read == file_content
+    result = read_file(file_name)
+    expected_result = file_content + append_content
+    assert result == expected_result
